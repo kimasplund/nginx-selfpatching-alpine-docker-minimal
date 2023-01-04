0a61b3207c6159ddd55ed4f60876b1f7355be970
Kim <kim.asplund@gmail.com>
2023-01-04 13:07:21 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+# Set log file path
+LOG_FILE="/var/log/self-upgrade.log"
+
 # Update package repository
-apk update
+apk update | tee -a "$LOG_FILE"
 
 # Upgrade installed packages
-apk upgrade
+apk upgrade | tee -a "$LOG_FILE"
 
 # Check for updated packages
 if [ $(apk info --installed | grep -vF "$(apk info --installed)" | wc -l) -gt 0 ]; then
   # Write log entry
-  echo "Upgraded packages: $(apk info --installed | grep -vF "$(apk info --installed)")" | tee -a /var/log/self-upgrade.log
+  echo "Upgraded packages: $(apk info --installed | grep -vF "$(apk info --installed)")" | tee -a "$LOG_FILE"
 fi
 
 # Check for updated services and restart them
@@ -22,14 +25,9 @@ for pkg in $(apk info --installed); do
     updated_version=$(apk info $pkg | grep '^$pkg-' | awk -F '-' '{print $2}')
     # Restart service if it is updated
     if [ "$current_version" != "$updated_version" ]; then
-      service $pkg restart
+      service $pkg restart | tee -a "$LOG_FILE"
       # Write log entry
-      echo "Restarted service: $pkg" | tee -a /var/log/self-upgrade.log
+      echo "Restarted service: $pkg" | tee -a "$LOG_FILE"
     fi
   fi
 done
-# Check if Nginx is running
-if ! pgrep -x "nginx" > /dev/null; then
-  # Start Nginx
-  nginx
-fi
